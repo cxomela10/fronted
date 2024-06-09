c96e92a3c124c05a7916e87935eb4888a9dba0a6
cxomela10 <118075111+cxomela10@users.noreply.github.com>
2024-06-09 21:38:33 +0400

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> DB_Hotel { get; set; }
+    }
+}
